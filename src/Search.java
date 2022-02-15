89c858b530d26fb2ddb4b695284a1d14cbb54182
BT_1E10_33 <sample@bths.edu>
2022-02-15 10:58:36 -0500

    Working on Search class

--- src/Search.java
+++ src/Search.java
@@ -1,31 +1,55 @@
 import java.util.ArrayList;
 
-public class Search {
+public class Search
+{
 
     // A. write a static method named linearSearch that returns the first index
     // a target int is found in an array of ints (both the target and int array
     // should be accepted as parameters); return -1 if target is not found
     // BE SURE TO USE EARLY RETURN (more efficient)
-
-
-
-
+    public static int linearSearch(int[] array, int target)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     // B. write a second static method that has the same parameters as linearSearch but
     // instead of returning an index, it returns true if target is found or false
     // if target is not found
     // AGAIN, BE SURE TO USE EARLY RETURN
-
-
-
-
+        public static boolean linearSearchFound(int[] array, int target)
+        {
+            for (int i = 0; i < array.length; i++)
+            {
+                if (array[i] == target)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
     // C. provide an OVERLOADED version of the linearSearch method that accepts an ArrayList
     // of Integers (rather than an array of ints) and a target int; return the first index
     // at which the target is found, or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
+        public static int linearSearch(ArrayList<Integer> array, int target)
+        {
+            for (int i = 0; i < array.size(); i++)
+            {
+                if (array.get(i) == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
 
 
@@ -33,24 +57,40 @@ public class Search {
     // of Strings and a target String; return the first index at which the target is found,
     // or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
-
-
+        public static int linearSearch(String[] array, String target)
+        {
+            for (int i = 0; i < array.length; i++)
+            {
+                if (array[i].equals(target))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
     // E. write a "linearSearchLast" method that accepts an array of ints and a
     // target int, and return the LAST index at which the target is found,
     // or return -1 if target is not found
+        public static int linearSearchLast(int[] array, int target)
+        {
+            int last = -1;
 
-
-
-
+            for (int i = 0; i < array.length; i++)
+            {
+                if (array[i] == target)
+                {
+                    last = i;
+                }
+            }
+            return last;
+        }
 
     // F. write two static "linearSearchCount" methods (both overloaded), one that
     // accepts an array of ints and a target int, and the other that accepts an ArrayList
     // of Integers and a target int.  Each method should return the NUMBER OF TIMES
     // the target appears in the array/ArrayList.
-
+        public static int linearSearchCount(ArrayList<Integer) 
 
 
 
--- src/SearchTest.java
+++ src/SearchTest.java
@@ -8,21 +8,21 @@ public class SearchTest {
 
         // TEST PART A
         // write test code to search numArray for 38 and print result; should print 3
-
+            System.out.println(Search.linearSearch(numArray, 38));
         // write test code to search numArray for 9 and print result; should print 2
-
+            System.out.println(Search.linearSearch(numArray, 9));
         // write test code to search numArray for 5 and print result; should print -1
-
+            System.out.println(Search.linearSearch(numArray, 5));
 
         // TEST PART B
         // use the same numArray and test the same three values as above, in the
         // same order, printing each result:  should print true, true, false
-
-
-
+            System.out.println(Search.linearSearchFound(numArray, 38));
+            System.out.println(Search.linearSearchFound(numArray, 9));
+            System.out.println(Search.linearSearchFound(numArray, 5));
 
         // TEST PARTS C & D -- UNCOMMENT TEST CODE
-        /*
+
         ArrayList<Integer> numArrayList = new ArrayList<Integer>(Arrays.asList(3, -2, 9, 38, -23, 9, 100));
         System.out.println("Tests of linear search Integer ArrayList:");
         System.out.println(Search.linearSearch(numArrayList, 3));
@@ -34,16 +34,16 @@ public class SearchTest {
         String[] arr1 = {"blue", "red", "purple", "green", "purple"};
         System.out.println(Search.linearSearch(arr1, "purple"));
         System.out.println(Search.linearSearch(arr1, "pink"));
-         */
+
 
         // TEST PART E -- UNCOMMENT TEST CODE
-        /*
+
         int[] numArray2 = {5, 1, 5, 4, 7, 5, 2, 5, 8, 8, 5, 10, 5};
         System.out.println(Search.linearSearchLast(numArray2, 4));
         System.out.println(Search.linearSearchLast(numArray2, 8));
         System.out.println(Search.linearSearchLast(numArray2, 5));
         System.out.println(Search.linearSearchLast(numArray2, 9));
-         */
+
 
         // TEST PART F -- UNCOMMENT TEST CODE
         /*
